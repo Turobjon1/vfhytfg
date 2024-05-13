aa8f80fbe88a43f0162908397086e68eba4f1ce2
Turobjon <avezovzamir337@gmail.com>
2024-05-13 16:05:38 +0500

    gfegfbgf

---
+++ script.js
@@ -0,0 +1,26 @@
+let Otadiv = document.getElementById('ota')
+let body = document.querySelector('body')
+
+let prt = prompt('soz kiriting')
+
+let h1 = document.createElement('h1')
+h1.innerHTML = prt
+
+h1.style.display = 'flex'
+h1.style.justifyContent = 'center'
+h1.style.alignItems = 'center'
+h1.style.height = '100vh'
+h1.style.color = 'green'
+body.style.backgroundColor = 'black'
+
+
+
+let div1 = document.getElementById('ota')
+
+Otadiv.appendChild(h1)
+
+
+
+
+
+
